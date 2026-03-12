eb1375229820be778a60c0e316227374120875a2
Leon-ahmed <mdleon1200@gmail.com>
2026-03-12 21:11:45 +0600

    Search Functionality and Design Done

--- Javascript/homepage.js
+++ Javascript/homepage.js
@@ -1,4 +1,6 @@
 
+
+// --------------ALL card------------
 let allCardsData = [];
 const loadCard=()=>{
     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
@@ -26,20 +28,7 @@ for(let card of cards){
 const Div=document.createElement("div");
 
 {
-// "id": 1,
-// "title": "Fix navigation menu on mobile devices",
-// "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-// "status": "open",
-// "labels": [
-// "bug",
-// "help wanted"
-// ],
-// "priority": "high",
-// "author": "john_doe",
-// "assignee": "jane_smith",
-// "createdAt": "2024-01-15T10:30:00Z",
-// "updatedAt": "2024-01-15T10:30:00Z"
-// },
+
 
 let border=card.status==="open"?"#00A96E":"#A855F7";
 let Clogo=card.status==="open"?"./assets/Open-Status.png":"./assets/Closed- Status .png";
@@ -111,8 +100,6 @@ Label += `
 
 };
 
-
-
 Div.innerHTML=`
 
 <div onclick="modalData(${card.id})" class="card w-11/12 mx-auto  min-h-[400px] bg-[#ffffff]  shadow-md  border-t-4 rounded-md" style="border-top-color:${border};">
@@ -149,23 +136,23 @@ container.append(Div);
 
 }
 
-
-
-
-
-
-
 }}
+// ------All Card End---------------------
 
-// filtering
+
+
+
+
+//--------------------- filtering  Based on status ----------------------
 
 document.getElementById("All").addEventListener("click",()=>{
+    bToggle("All");
   spinner(false);
  setTimeout(()=>displayCard(allCardsData), 300);
 
 });
 document.getElementById("Open").addEventListener("click",()=>{
-   
+   bToggle("Open");
   spinner(false);
   const openData=allCardsData.filter(card=>card.status=="open");
    setTimeout(()=>displayCard(openData), 300);
@@ -173,6 +160,7 @@ document.getElementById("Open").addEventListener("click",()=>{
 
 });
 document.getElementById("Close").addEventListener("click",()=>{
+  bToggle("Close");
    spinner(false);
   const closeData=allCardsData.filter(card=>card.status=="closed");
    setTimeout(()=>displayCard(closeData), 300);
@@ -181,15 +169,14 @@ document.getElementById("Close").addEventListener("click",()=>{
 });
 
 
-
-
-
-
 loadCard();
+//--------------------- filtering  End ----------------------
 
 
 
-// Button Toggle
+
+
+// ----------------Button Toggle------------------
 function bToggle(Id){
 const ids=["All","Open","Close"];
 
@@ -207,9 +194,14 @@ for(const id of ids){
   }
 }
 };
+// ----------------Button Toggle   End------------------
 
 
-// Spinner 
+
+
+
+
+// ------------------Spinner --------------------
 
 const spinner=(Status)=>{
 
@@ -224,14 +216,16 @@ document.getElementById("allCard").classList.add("hidden");
 
 }
 
-
-
 }
+// ------------Spinner End------------- 
 
 
 
 
-// ModalDisplay
+
+
+
+//---------------------- ModalDisplay--------------------------
 
 const modalData=async(id)=>{
 const url=`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
@@ -248,30 +242,8 @@ if(details && details.data){
 }
 
 
-// "status": "success",
-// "message": "Issue fetched successfully",
-// "data": {
-// "id": 33,
-// "title": "Add bulk operations support",
-// "description": "Allow users to perform bulk actions like delete, update status on multiple items at once.",
-// "status": "open",
-// "labels": [
-// "enhancement"
-// ],
-// "priority": "low",
-// "author": "bulk_barry",
-// "assignee": "",
-// "createdAt": "2024-02-02T10:00:00Z",
-// "updatedAt": "2024-02-02T10:00:00Z"
-// }
-// }
-
-
 
  const displayModal=(card)=>{
-
-
-
 
 let priorityBG="";
 let priorityTxt="";
@@ -404,3 +376,58 @@ detailCard.innerHTML=`
 
  document.getElementById("my_modal_4").showModal();
  };
+// -----------------Modal End----------------------
+
+
+
+
+
+
+
+
+
+
+// ---------------------- Search ------------------------------------
+
+
+document.getElementById("Search").addEventListener("click", () => {
+  const ids = ["All","Open","Close"];
+
+  for (const id of ids) {
+    const button = document.getElementById(id);
+    if (!button) continue;
+
+    button.classList.remove("Active");
+    button.classList.add("Remove");
+  }
+});
+
+
+
+document.getElementById("searchbtn").addEventListener("click", () => {
+
+
+  const searchInput = document.getElementById("Search");
+  const searchValue = searchInput.value.trim().toLowerCase();
+
+  console.log(searchValue);
+
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+
+      const SearchWord = data.data;
+      console.log(SearchWord);
+
+      const filterWord = SearchWord.filter((word) =>
+        word.title.toLowerCase().includes(searchValue)
+      );
+
+      displayCard(filterWord);
+    });
+
+});
+
+// Search------------End-------------------
